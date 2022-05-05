40c42b138578dcf5add973f3028e08883bbe2454
dianabanguera <dianabangueravillalobo@gmail.com>
2022-05-05 13:40:50 -0500

    task 0

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -2,11 +2,10 @@
 def add(a, b):
     """My addition function
 
-
     Args:
         a: first integer
         b: second integer
     Returns:
         The return value. a + b
     """
-    return (a + b)i
+    return (a + b)
