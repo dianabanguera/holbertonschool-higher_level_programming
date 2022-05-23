2e755029da528fcce0241faef14a43c39a774d37
dianabanguera <dianabangueravillalobo@gmail.com>
2022-05-23 15:57:00 -0500

    Task4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -2,7 +2,7 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    
+
     for index in range(list_length):
         try:
             result = my_list_1[index] / my_list_2[index]
