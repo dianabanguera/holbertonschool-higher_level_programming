903bd1766ac2d898cabbf68d1c4ccaccd38af20a
dianabanguera <dianabangueravillalobo@gmail.com>
2022-05-24 15:47:02 -0500

    My first commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Define an empty Square Class"""
+
+
+class Square:
+    """An empty class"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a square class"""
+
+
+class Square:
+    """Represents the atributtes 
+    
+    of the square
+    __size (int): size
+    """
+    def __init__(self, size):
+        
+        self.__size = size
