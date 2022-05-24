323b3c4ae16245735fc9f1f04f3253057998aa45
dianabanguera <dianabangueravillalobo@gmail.com>
2022-05-24 15:52:57 -0500

    task1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,12 +3,9 @@
 
 
 class Square:
-    """Represents the atributtes 
-    
+    """Represents the atributtes
     of the square
     __size (int): size
     """
     def __init__(self, size):
-        """ Initialize a square"""
-        
         self.__size = size
