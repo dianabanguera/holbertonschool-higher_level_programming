f65a911ff7059f646b9d724f795d3aa330588424
dianabanguera <dianabangueravillalobo@gmail.com>
2022-05-24 19:46:34 -0500

    Task6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""Define a square class"""
+
+
+class Square:
+    """Represents the atributtes
+    of the square
+    __size (int): size
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize the square
+
+        Args:
+            size (int): size of the square
+            position: the position of the square
+        """
+        self.__size = size
+        self.position = position
+
+    @property
+    def position(self):
+        """int: Return the position of the square"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Sets the attribute attribute"""
+        self.__position = value
+        if type(value) is not tuple:
+            self.__position = None
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif len(value) != 2:
+            self.__position = None
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) is not int or type(value[1]) is not int:
+            self.__position = None
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            self.__position = None
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    @property
+    def size(self):
+        """int: Return the size of the square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Sets the attribute size"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("value must be >= 0")
+
+    def area(self):
+        """int: Return the area of the square"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """Prints the square
+        with the symbol '#'"""
+        if self.__size != 0:
+            print()
+            return
+        for i in range(self.__position[1]):
+            print()
+        for j in range(self.__size):
+            print("{}{}".format(" " * self.position[0], "#" * self.__size))
