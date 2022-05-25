01b44c9abc40aa17dcb1fa42492a10246f49ef5d
dianabanguera <dianabangueravillalobo@gmail.com>
2022-05-24 19:53:54 -0500

    Task6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -9,12 +9,11 @@ class Square:
     """
     def __init__(self, size=0, position=(0, 0)):
         """Initialize the square
-
         Args:
             size (int): size of the square
             position: the position of the square
         """
-        self.__size = size
+        self.size = size
         self.position = position
 
     @property
@@ -42,28 +41,27 @@ class Square:
     @property
     def size(self):
         """int: Return the size of the square"""
-        return (self.__size)
+        return self.__size
 
     @size.setter
     def size(self, value):
         """Sets the attribute size"""
+        self.__size = value
         if type(value) is not int:
             raise TypeError("size must be an integer")
-        else:
-            if value < 0:
-                raise ValueError("value must be >= 0")
+        if value < 0:
+            raise ValueError("size must be >= 0")
 
     def area(self):
         """int: Return the area of the square"""
-        return (self.__size ** 2)
+        return (self.__size) ** 2
 
     def my_print(self):
-        """Prints the square
-        with the symbol '#'"""
-        if self.__size != 0:
+        """Prints the square with the symbol '#'"""
+        if self.__size == 0:
             print()
             return
         for i in range(self.__position[1]):
             print()
-        for j in range(self.__size):
+        for x in range(self.__size):
             print("{}{}".format(" " * self.position[0], "#" * self.__size))
