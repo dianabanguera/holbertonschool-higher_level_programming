10ab230fd99531d87f3f71a30037eeeb481f41db
dianabanguera <dianabangueravillalobo@gmail.com>
2022-05-24 19:34:44 -0500

    Task5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Define a square class"""
+
+
+class Square:
+    """Represents the atributtes
+    of the square
+    __size (int): size
+    """
+    def __init__(self, size=0):
+        """Initialize the square
+
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """int: Return the size of the square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Sets the attribute size"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("value must be >= 0")
+            else:
+                self.__size = value
+
+    def area(self):
+        """int: Return the area of the square"""
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """Prints the square
+        with the symbol '#'"""
+        if self.__size != 0:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print('#', end="")
+                print("")
+        else:
+            print("")
