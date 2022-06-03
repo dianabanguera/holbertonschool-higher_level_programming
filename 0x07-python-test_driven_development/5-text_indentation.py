93a3b5682e814feda3aa5387f5ba8592027e8fd0
dianabanguera <dianabangueravillalobo@gmail.com>
2022-06-02 21:21:16 -0500

    task5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+The "5-text_indentation" module
+This module supplies one function, text_indentation().
+"""
+
+
+def text_indentation(text):
+    """Prints a text with 2 new lines after each of these characters:
+    ('.', '?' and ':')
+    Print some error if something is wrong
+    """
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+    print(text, end="")
