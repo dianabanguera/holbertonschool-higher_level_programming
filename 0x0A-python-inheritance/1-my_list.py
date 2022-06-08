a6ea0b17899a4f16c1e4aaceae1996c10f222fe4
dianabanguera <dianabangueravillalobo@gmail.com>
2022-06-07 21:14:55 -0500

    Task1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""My list class"""
+
+
+def Mylist(list):
+    """Another class - subclass from list"""
+
+    def print_sorted(self):
+        """Prints self"""
+
+        print(sorted(self))
