23b7061462482469e2fa69878268b10433df8f50
dianabanguera <dianabangueravillalobo@gmail.com>
2022-06-08 20:23:07 -0500

    from json string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""from_json_string"""
+import json
+
+
+def from_json_string(my_str):
+    """ function that returns an object
+    represented by a JSON string"""
+    return json.loads(my_str)
