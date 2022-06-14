b6bc52f246050c914ce2250195d1b823ed98aa98
dianabanguera <dianabangueravillalobo@gmail.com>
2022-06-14 10:06:48 -0500

    json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Base class"""
+from fileinput import filename
+import json
 
 
 class Base:
@@ -12,3 +14,34 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+        if list_objs is None or list_objs == []:
+            json_s = "[]"
+        else:
+            json_s = cls.to_json_string([element.to_dictionary() for element in list_objs])
+        filename = f"{cls.__name__}.json"
+        with open(filename, "w") as file:
+            file.write(json_s)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation json_string"""
+        if json_string is None or json_string == []:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set"""
