aeb3c3ec6fe543ca19cef06531d7d4b36a5d5717
dianabanguera <dianabangueravillalobo@gmail.com>
2022-07-13 21:14:20 -0500

    script that converts hbtn_0c_0 database to UTF8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,6 +2,7 @@
 -- converts Database hbtn_0c_0 to utf8
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
 -- converts Table first_table to utf8
-ALTER TABLE hbtn_0c_0.first_table CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+ALTER TABLE first_table CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
 -- converts Field name in first_table to utf8
-ALTER TABLE hbtn_0c_0.first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 collate utf8mb4_unicode_ci;
