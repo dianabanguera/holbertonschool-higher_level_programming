b544db448afeb63bdd2236aa81269c445070be76
dianabanguera <dianabangueravillalobo@gmail.com>
2022-07-14 16:09:32 -0500

    lists all shows and all genres linked to that show

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,6 +4,6 @@
 -- Results must be sorted in ascending order by the show title and genre name.
 
 SELECT title, name FROM tv_shows
-INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY title ASC, name ASC;
