8b7e8c62cbb94d02c30e331bbcde7ff0a29a14ee
dianabanguera <dianabangueravillalobo@gmail.com>
2022-07-14 15:02:08 -0500

    lists all cities contained in the database usa

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -2,4 +2,4 @@
 -- Each record should display: cities.id - cities.name - states.name
 -- Results must be sorted in ascending order by cities.id.
 
-SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY ASC;
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id;
