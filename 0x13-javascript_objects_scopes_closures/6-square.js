0019c32b55ca802cd670973b93e3dc488ea643a2
dianabanguera <dianabangueravillalobo@gmail.com>
2022-08-16 15:18:42 -0500

    instance method called charPrint(c)

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,7 +4,7 @@ const square = require('./5-square');
 class Square extends square {
   charPrint (c) {
     if (c === undefined) {
-      for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+      this.print();
     } else {
       for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
