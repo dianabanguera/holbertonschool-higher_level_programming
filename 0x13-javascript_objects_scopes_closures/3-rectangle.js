8a111a4f31cd9aabd18f7334f28877919bafc4b8
dianabanguera <dianabangueravillalobo@gmail.com>
2022-08-16 14:21:21 -0500

    Create an instance method called print()

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,8 +8,8 @@ class Rectangle {
   }
 
   print () {
-    for (let i = 0; i < this.height; i++) { 
-      console.log('X'.repeat(this.width)); 
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
 }
