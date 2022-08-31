135d557a13a5d4cc16b445aff6a4d21c69be8162
dianabanguera <dianabangueravillalobo@gmail.com>
2022-08-30 19:57:09 -0500

    First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+model_state.py module
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Inherit class definition for new table.
+    Attributes:
+        -id
+        -name
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
