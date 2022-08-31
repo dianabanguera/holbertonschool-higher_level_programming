03c91393fe81d4a4354c1786d92e2caeb8c8d086
dianabanguera <dianabangueravillalobo@gmail.com>
2022-08-30 21:34:21 -0500

    prints all City objects from the db hbtn_0e_14_usa

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""
+model_city.py module
+"""
+from model_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class City(Base):
+    """
+    Inherit class definition for new table.
+    Attributes:
+        -id
+        -name
+        -state_id
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
