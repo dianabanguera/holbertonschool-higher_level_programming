e1647d98dc670a31a937d9373d114d5187a12dfb
dianabanguera <dianabangueravillalobo@gmail.com>
2022-09-02 10:56:19 -0500

    My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""script that takes your GitHub
+credentials (username and password)
+and uses the GitHub API to display
+your id"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/users/{}'.format(argv[1])
+    headers = {'Authorization': 'token ' + argv[2]}
+    response = requests.get(url, headers=headers)
+    print(response.json().get('id'))
