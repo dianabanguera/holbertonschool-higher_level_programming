8b6f60e2194145adeadcb7e7bca9d738eadbb189
dianabanguera <dianabangueravillalobo@gmail.com>
2022-09-02 10:54:24 -0500

    Search API

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
-"""script that takes in a URL,
-sends a request to the URL and
-displays the value of the X-Request-Id
-variable found in the header of the response."""
+"""takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user
+with the letter as a parameter"""
 
-if __name__ == "__main__":
-    import urllib.request as request
+if __name__ == '__main__':
+    import requests
     from sys import argv
-    req = request.Request(argv[1])
-    with request.urlopen(req) as r:
-        print(r.headers.get('X-Request-Id'))
+    if len(argv) == 2:
+        q = argv[1]
+    else:
+        q = ""
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        r_dict = r.json()
+        id = r_dict.get('id')
+        name = r_dict.get('name')
+        if len(r_dict) == 0 or not id or not name:
+            print("No result")
+        else:
+            print("[{}] {}".format(r_dict.get('id'), r_dict.get('name')))
+    except:
+        print("Not a valid JSON")
